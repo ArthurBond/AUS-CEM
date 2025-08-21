691311eac48f2872ab70573e2937d6c280d8da25
Arthur Bond <u7299997@anu.edu.au>
2025-08-21 13:19:59 +1000

    added new IASR 23 sheets and script templates

---
+++ scripts/build_isp24_network.py
@@ -0,0 +1,9 @@
+# too difficult to do lead times I think (cost should be most of it)
+# how to do system strength cost ?
+
+# hydrogen out of scope
+
+# add (initial) build limits (p_nom_extendable = True and p_nom_max = build limit for region)
+# these limits can be augmented with REZ augmentations
+
+# add MLFs -> not the best assumption bc these change as generation cluster changes
---
+++ scripts/write_line_constraints.py
@@ -0,0 +1,27 @@
+from linopy import Variable
+
+def add_line_constraints(n):
+
+    # Suppose options is a list of expansion options like ["NQ-CQ_aug1", "NQ-CQ_aug2"]
+    x = n.model.add_variables(lower=0, upper=1, binary=True, name="augment_option")
+
+    link = "NQ-CQ"
+    Δf = {"aug1": 500, "aug2": 1000}  # MW increments
+
+    link_flow = n.model["Link-p"].loc[:, link]
+
+    # Check if this works without augmentation bit
+
+    # Implement with timeslice to get hot days
+
+    for aug, cap in Δf.items():
+        n.model.add_constraints(
+            link_flow <= n.links.at[link, "p_nom"] + cap * x.loc[aug]
+        )
+        n.model.add_constraints(
+            link_flow >= -n.links.at[link, "p_nom"] - cap * x.loc[aug]
+        )
+
+    # Some augmentations only occur if others have occured
+
+    n.model.add_constraints(x.loc["aug2"] <= x.loc["aug1"])
